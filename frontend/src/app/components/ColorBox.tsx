f8e83cc45de4ebc9ad7a9491ee0bbe2af7cd6a84
hilton326 <hjd59728@uga.edu>
2025-04-09 23:41:30 -0400

    UI mostly done

--- frontend/src/app/components/ColorBox.tsx
+++ frontend/src/app/components/ColorBox.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface props {
     hexCode?: string;
 }
@@ -6,16 +8,35 @@ export default function ColorBox({hexCode}: props) {
 
     const color = hexCode;
     console.log(color);
+    let generated = (color != "");
+
     return (
         <div>
-            <div className="text-white">
-                <h1> {color} </h1>
-            </div>
-            <div
-            className="w-48 h-48 text-black border-4 border-black"
-            style={{ backgroundColor: color }}
-            >
-            </div>
+            {generated ? (
+                <div>
+                    <div className="text-white text-center font-bold">
+                        <h1> {color} </h1>
+                    </div>
+                    <div className="w-48 h-48 border-black border-4 rounded-3xl" style={{ backgroundColor: color }}>
+                    </div>
+                </div>
+            ) : (
+              <div>
+                <div className="text-white text-center font-bold">
+                    <h1> Color name </h1>
+                </div>
+                <div className="w-48 h-48 border-black border-4 rounded-3xl bg-[#313131] flex">
+                    <Image
+                        src="/images/EmptySymbol.png"
+                        width={100}
+                        height={100}
+                        className="m-auto"
+                        alt="no color yet"
+                    />
+                </div>
+              </div>
+            )}
+
         </div>
     );
     
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,30 +4,31 @@ import Button from "./components/Button";
 import ColorBox from "./components/ColorBox";
 import NavBar from "./components/NavBar";
 
-async function getPostByHex() {
+/*async function getPostByHex() {
   const response = await fetch('https://www.thecolorapi.com/id?hex=bb0000');
   return response.json();
-}
+}*/
+
 export default async function Home() {
 
-  const post = await getPostByHex()
+  //const post = await getPostByHex()
   return (
     <div className="bg-[#1e1e1e] grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <NavBar />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="flex flex-col">
           <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <ColorBox hexCode="#ff0000"/>
-            <ColorBox hexCode="#00ff00"/>
-            <ColorBox hexCode="#0000ff"/>
-            <ColorBox hexCode="#000000"/>
+          
             <ColorBox hexCode="#ffffff"/>
+            <ColorBox hexCode="#ff00ff"/>
+            <ColorBox hexCode="#abcdef"/>
+            <ColorBox hexCode="#123456"/>
+            <ColorBox hexCode=""/>
           </div>
-          <div className="ml-auto mr-auto mt-10 items-center">
-            <Button label="Generate" />
-          </div>
-          <div className="ml-auto mr-auto mt-10 items-center">
-            <Button label="Saved Palettes" />
+          <div className="ml-auto mr-auto mt-16 items-center">
+            <button className="rounded-3xl text-4xl font-bold bg-white text-black px-12 py-4 border-2 border-white mr-5">
+              Generate
+            </button>
           </div>
         </div>
       </main>
